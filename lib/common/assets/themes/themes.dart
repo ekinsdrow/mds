a4826d612c9014172a018ce28ece68a5cc421779
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-10 19:26:38 +0300

    Add button theme

--- lib/common/assets/themes/themes.dart
+++ lib/common/assets/themes/themes.dart
@@ -35,6 +35,20 @@ abstract class Themes {
         checkboxTheme: CheckboxThemeData(
           fillColor: MaterialStateProperty.all(AppColors.primaryColor),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: AppColors.primaryColor,
+            padding: const EdgeInsets.symmetric(
+              vertical: 5,
+              horizontal: Constants.mediumPadding * 2,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                Constants.borderRadius * 1.5,
+              ),
+            ),
+          ),
+        ),
       );
 
   static final InputBorder _inputBorder = OutlineInputBorder(
