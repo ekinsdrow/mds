afdaebc48488d73e014f63072a25dbe8c733bc60
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-10 19:45:44 +0300

    start create settings ui

--- lib/features/main/settings/widgets/settings_page.dart
+++ lib/features/main/settings/widgets/settings_page.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mds/common/assets/constants.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Settings'),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(
+          Constants.mediumPadding,
+        ),
+        child: Column(
+          children: [
+            //TODO: settings
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  //TODO: remove all downloaded files
+                },
+                child: Text(
+                  AppLocalizations.of(context)!.remove_all_downloaded_files,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
