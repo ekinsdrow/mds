7fbd4bf809666aaa02fb0216ce6ce93c7953a359
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-18 17:55:22 +0300

    Add error snackbar

--- lib/features/playing/di/playing_scope.dart
+++ lib/features/playing/di/playing_scope.dart
@@ -1,8 +1,11 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mds/common/widgets/error_snackbar.dart';
 import 'package:mds/features/playing/logic/audio_handler.dart';
 import 'package:provider/provider.dart';
 
-class PlayingScope extends StatelessWidget {
+class PlayingScope extends StatefulWidget {
   const PlayingScope({
     required this.child,
     required this.mdsAudioHandler,
@@ -13,10 +16,31 @@ class PlayingScope extends StatelessWidget {
   final MdsAudioHandler mdsAudioHandler;
 
   @override
+  State<PlayingScope> createState() => _PlayingScopeState();
+}
+
+class _PlayingScopeState extends State<PlayingScope> {
+  @override
+  void initState() {
+    super.initState();
+
+    widget.mdsAudioHandler.playbackState.listen(
+      (value) {
+        if (value.processingState == AudioProcessingState.error) {
+          showErrorSnackBar(
+            context: context,
+            message: AppLocalizations.of(context)!.playing_error,
+          );
+        }
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Provider.value(
-      value: mdsAudioHandler,
-      child: child,
+      value: widget.mdsAudioHandler,
+      child: widget.child,
     );
   }
 }
