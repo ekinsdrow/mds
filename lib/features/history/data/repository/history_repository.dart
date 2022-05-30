593578050bd542aed3169de440dec099aaf82d77
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-30 12:41:09 +0300

    Add history repo

--- lib/features/history/data/repository/history_repository.dart
+++ lib/features/history/data/repository/history_repository.dart
@@ -1,7 +1,36 @@
-import 'package:mds/features/history/data/models/history_record.dart';
+import 'package:mds/common/data/models/record.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class IHistoryRepository {
-  Future<int> saveHistoryRecord(HistoryRecord record);
+  Future<void> saveHistoryRecord(Record record);
   Future<void> clearHistory();
-  Future<List<HistoryRecord>> getHistory();
+  Future<List<String>> getHistoryIds();
+}
+
+class HistoryRepository implements IHistoryRepository {
+  final _key = 'history';
+
+  @override
+  Future<void> clearHistory() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.remove(_key);
+  }
+
+  @override
+  Future<List<String>> getHistoryIds() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final histories = sharedPreferences.getStringList(_key);
+    return histories ?? [];
+  }
+
+  @override
+  Future<void> saveHistoryRecord(Record record) async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final history = [
+      ...await getHistoryIds(),
+      record.recordId,
+    ];
+
+    sharedPreferences.setStringList(_key, history);
+  }
 }
