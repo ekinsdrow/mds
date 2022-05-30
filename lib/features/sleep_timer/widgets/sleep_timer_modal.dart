5ea4eaf0bb09c3a9548f29c1803dca1e86ba3e80
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-30 15:14:48 +0300

    Remove unused import

--- lib/features/sleep_timer/widgets/sleep_timer_modal.dart
+++ lib/features/sleep_timer/widgets/sleep_timer_modal.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mds/common/assets/constants.dart';
-import 'package:mds/features/sleep_timer/logic/sleep_timer.dart';
 
 void showSleepTimerModal(BuildContext context) {
   showModalBottomSheet(
