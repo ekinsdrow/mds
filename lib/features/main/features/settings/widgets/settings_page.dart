0493aaf2fe57bbf2c62380546a7692a50fe84e49
Ivan Kolchev <ekinsdrow@gmail.com>
2022-05-30 13:13:37 +0300

    Add clear history button

--- lib/features/main/features/settings/widgets/settings_page.dart
+++ lib/features/main/features/settings/widgets/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mds/common/assets/constants.dart';
+import 'package:mds/features/history/bloc/history_bloc/history_bloc.dart';
+import 'package:provider/provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -23,6 +25,19 @@ class SettingsPage extends StatelessWidget {
                 },
                 child: Text(
                   AppLocalizations.of(context)!.remove_all_downloaded_files,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  context.read<HistoryBloc>().add(
+                        const HistoryEvent.clearAll(),
+                      );
+                },
+                child: Text(
+                  AppLocalizations.of(context)!.remove_all_history,
                 ),
               ),
             ),
